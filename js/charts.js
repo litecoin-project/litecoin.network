942184af8bb32f9342e49769e3bd654c16f51a8c
Frank <MLPFrank@live.com>
2017-08-03 15:47:29 +0100

    Minor Update to UI

--- js/charts.js
+++ js/charts.js
@@ -1,15 +1,17 @@
+
+
 const CHART = document.getElementById("LineChart");
 console.log(CHART);
 
 let LineChart = new Chart(CHART, {
 	type:'line',
 	data: {
-    labels: ["January", "February", "March", "April", "May", "June", ],
+    labels: ["01.07.17", "02.07.17", "03.07.17", "04.07.17", "05.07.17", "06.07.17", "07.07.17", "08.07.17", "09.07.17", "10.07.17", "11.07.17", "12.07.17", "12.07.17", "13.07.17","14.07.17", "15.07.17", "16.07.17", "17.07.17", "18.07.17", "19.07.17", "20.07.17",],
 		datasets: [
 			{
 				label: "Market Price ($USD)",
 				fill: false,
-				lineTension: 0.1,
+				lineTension: 0.2,
 				backgroundColor: "rgba(250, 176, 143, 1)",
 				borderColor: "rgba(250, 176, 143, 1)",
 				borderCapStyle: 'butt',
@@ -25,8 +27,8 @@ let LineChart = new Chart(CHART, {
 				pointHoverBorderWidth: 2,
 				pointRadius: 1,
 				pointHitRadius: 10,
-				data: [4, 4, 4, 16, 56, 100],
-				spanGaps: false,
+				data: [21.53, 22.31, 20.98, 21.72, 23.01, 23.56, 24.75, 26.42, 25.83, 25.80, 26, 25.50, 26.87, 28.52, 29.73, 31.21, 33, 32.4, 33, 28.99,27],
+				spanGaps: true,
 			}
 		]
 	}
@@ -54,19 +56,19 @@ let BarChart = new Chart(BARCHART, {
 				pointBackgroundColor: "#fff",
 				pointBorderWidth: 1,
 				pointHoverRadius: 5,
-				pointHoverBackgroundColor:  [ 
-					"rgba(255, 243, 183, 0.8)", 
-					"rgba(249, 204, 132, 0.8)", 
-					"rgba(250, 176, 143, 0.8)", 
-					"rgba(240, 158, 145, 0.8)", 
-					"rgba(243, 129, 112, 0.8)", 
+				pointHoverBackgroundColor:  [
+					"rgba(255, 243, 183, 0.8)",
+					"rgba(249, 204, 132, 0.8)",
+					"rgba(250, 176, 143, 0.8)",
+					"rgba(240, 158, 145, 0.8)",
+					"rgba(243, 129, 112, 0.8)",
 					"rgba(255, 111, 117, 0.8)",
 					"rgba(211, 102, 101, 0.8)",
 					"rgba(180, 100, 123, 0.8)",
 					"rgba(229, 150, 214, 0.8)",
 					"rgba(192, 129, 223, 0.8)",
 					"rgba(234, 146, 165, 0.8)",
-					"rgba(255, 144, 168, 0.8)"			
+					"rgba(255, 144, 168, 0.8)"
 				],
 				pointHoverBorderColor: "rgba(220,220,220,1)",
 				pointHoverBorderWidth: 2,
@@ -91,19 +93,19 @@ let PieChart = new Chart(PIECHART, {
 				label: "Litecoin Price ($USD)",
 				fill: false,
 				lineTension: 0.1,
-				backgroundColor:  [ 
-					"rgba(255, 243, 183, 0.8)", 
-					"rgba(249, 204, 132, 0.8)", 
-					"rgba(250, 176, 143, 0.8)", 
-					"rgba(240, 158, 145, 0.8)", 
-					"rgba(243, 129, 112, 0.8)", 
+				backgroundColor:  [
+					"rgba(255, 243, 183, 0.8)",
+					"rgba(249, 204, 132, 0.8)",
+					"rgba(250, 176, 143, 0.8)",
+					"rgba(240, 158, 145, 0.8)",
+					"rgba(243, 129, 112, 0.8)",
 					"rgba(255, 111, 117, 0.8)",
 					"rgba(211, 102, 101, 0.8)",
 					"rgba(180, 100, 123, 0.8)",
 					"rgba(229, 150, 214, 0.8)",
 					"rgba(192, 129, 223, 0.8)",
 					"rgba(234, 146, 165, 0.8)",
-					"rgba(255, 144, 168, 0.8)"			
+					"rgba(255, 144, 168, 0.8)"
 				],
 				borderColor: "rgba(161, 78, 104, 1)",
 				borderCapStyle: 'butt',
@@ -148,19 +150,19 @@ let RadarChart = new Chart(RADARCHART, {
 				pointBackgroundColor: "rgba(161, 78, 104, 0.8)",
 				pointBorderWidth: 1,
 				pointHoverRadius: 5,
-				pointHoverBackgroundColor: [ 
-					"rgba(255, 243, 183, 0.8)", 
-					"rgba(249, 204, 132, 0.8)", 
-					"rgba(250, 176, 143, 0.8)", 
-					"rgba(240, 158, 145, 0.8)", 
-					"rgba(243, 129, 112, 0.8)", 
+				pointHoverBackgroundColor: [
+					"rgba(255, 243, 183, 0.8)",
+					"rgba(249, 204, 132, 0.8)",
+					"rgba(250, 176, 143, 0.8)",
+					"rgba(240, 158, 145, 0.8)",
+					"rgba(243, 129, 112, 0.8)",
 					"rgba(255, 111, 117, 0.8)",
 					"rgba(211, 102, 101, 0.8)",
 					"rgba(180, 100, 123, 0.8)",
 					"rgba(229, 150, 214, 0.8)",
 					"rgba(192, 129, 223, 0.8)",
 					"rgba(234, 146, 165, 0.8)",
-					"rgba(255, 144, 168, 0.8)"			
+					"rgba(255, 144, 168, 0.8)"
 				],
 				pointHoverBorderColor: "rgba(161, 78, 104,1)",
 				pointHoverBorderWidth: 2,
@@ -185,19 +187,19 @@ let PolarChart = new Chart(POLARCHART, {
 				label: "Litecoin Price ($USD)",
 				fill: false,
 				lineTension: 0.1,
-				backgroundColor: [ 
-					"rgba(255, 243, 183, 0.8)", 
-					"rgba(249, 204, 132, 0.8)", 
-					"rgba(250, 176, 143, 0.8)", 
-					"rgba(240, 158, 145, 0.8)", 
-					"rgba(243, 129, 112, 0.8)", 
+				backgroundColor: [
+					"rgba(255, 243, 183, 0.8)",
+					"rgba(249, 204, 132, 0.8)",
+					"rgba(250, 176, 143, 0.8)",
+					"rgba(240, 158, 145, 0.8)",
+					"rgba(243, 129, 112, 0.8)",
 					"rgba(255, 111, 117, 0.8)",
 					"rgba(211, 102, 101, 0.8)",
 					"rgba(180, 100, 123, 0.8)",
 					"rgba(229, 150, 214, 0.8)",
 					"rgba(192, 129, 223, 0.8)",
 					"rgba(234, 146, 165, 0.8)",
-					"rgba(255, 144, 168, 0.8)"			
+					"rgba(255, 144, 168, 0.8)"
 				],
 				borderColor: "rgba(161, 78, 104,1)",
 				borderCapStyle: 'butt',
@@ -232,19 +234,19 @@ let DoughnutChart = new Chart(DOUGHNUTCHART, {
 				label: "Litecoin Price ($USD)",
 				fill: false,
 				lineTension: 0.1,
-				backgroundColor: [ 
-					"rgba(255, 243, 183, 0.8)", 
-					"rgba(249, 204, 132, 0.8)", 
-					"rgba(250, 176, 143, 0.8)", 
-					"rgba(240, 158, 145, 0.8)", 
-					"rgba(243, 129, 112, 0.8)", 
+				backgroundColor: [
+					"rgba(255, 243, 183, 0.8)",
+					"rgba(249, 204, 132, 0.8)",
+					"rgba(250, 176, 143, 0.8)",
+					"rgba(240, 158, 145, 0.8)",
+					"rgba(243, 129, 112, 0.8)",
 					"rgba(255, 111, 117, 0.8)",
 					"rgba(211, 102, 101, 0.8)",
 					"rgba(180, 100, 123, 0.8)",
 					"rgba(229, 150, 214, 0.8)",
 					"rgba(192, 129, 223, 0.8)",
 					"rgba(234, 146, 165, 0.8)",
-					"rgba(255, 144, 168, 0.8)"			
+					"rgba(255, 144, 168, 0.8)"
 				],
 				borderColor: "rgba(161, 78, 104,1)",
 				borderCapStyle: 'butt',
